cfac50070dc1859a3a987a291467030b0cd0eeb6
anupdass <anupdas633@gmail.com>
2024-08-13 17:08:10 +0600

    added nav menu for mobile view

---
+++ app/component/DropDownMenu.jsx
@@ -0,0 +1,64 @@
+'use client'
+import React, { useEffect, useState } from "react";
+
+import { navData } from "../data/navdata";
+import NavItem from "./NavItem";
+import Link from "next/link";
+
+export function DropdownMenu({ openNav, handleOpenNav }) {
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (!event.target.closest('.dropdown-menu') && openNav) {
+                handleOpenNav(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            // Cleanup the event listener on component unmount
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [openNav, handleOpenNav]);
+
+    return (
+        <>
+            {
+                openNav ?
+                    <div className='flex flex-col gap-6 p-7'>
+                        {
+                            navData.map((item) => {
+                                return (
+                                    <Link key={item.id} href={item.path}
+                                        onClick={() => handleOpenNav(false)}
+                                    >
+                                        {item.name}
+                                    </Link>
+                                )
+                            })
+                        }
+                        <div>
+
+                            <button
+                                className='px-4 py-2 bg-[#1c9a72] 
+                    rounded-md text-white hover:bg-[#2cbc8f]
+                    hover:scale-105 transition-all duration-300 mr-5 md:mr-0'
+                            >
+                                Pay-Bill
+                            </button>
+                            <button
+                                className='px-4 py-2 bg-[#1c9a72] 
+                    rounded-md text-white hover:bg-[#2cbc8f]
+                    hover:scale-105 transition-all duration-300 mr-5 md:mr-0'
+                            >
+                                Self-Care
+                            </button>
+                        </div>
+                    </div>
+                    :
+                    null
+            }
+        </>
+    );
+}
--- app/component/Navbar.jsx
+++ app/component/Navbar.jsx
@@ -1,22 +1,39 @@
-
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import { navData } from '../data/navdata'
 import NavItem from './NavItem'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { DropdownMenu } from './DropDownMenu'
+import { IconButton } from '@material-tailwind/react'
+
 
 const Navbar = () => {
     const [currentHash, setCurrentHash] = useState('Home');
+    const [openNav, setOpenNav] = useState(false);
 
     const changeHash = (pathname) => {
         setCurrentHash(pathname)
     }
 
+    const handleOpenNav = () => {
+        setOpenNav(!openNav)
+    }
+
+    useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => window.innerWidth >= 960 && setOpenNav(false),
+        );
+    }, []);
+
+
+
     return (
         <div className='shadow-md sticky top-0 z-50 w-full bg-white '>
+
             <nav className='flex justify-between custom-container '>
-                <Link href="#home">
+                <Link href="#home" className='px-3 md:px-0'>
                     <Image src="/udoyLogo.png" alt="logo" width={150} height={150} priority={true} onClick={() => setCurrentHash('Home')} />
                 </Link>
                 <div className='hidden md:block'>
@@ -36,15 +53,68 @@ const Navbar = () => {
                         }
                     </div>
                 </div>
-                <button
-                    className='px-4 py-2 bg-[#1c9a72] 
+                <div className='flex gap-3'>
+                    <button
+                        className='px-4 py-2 bg-[#1c9a72] 
                     rounded-md text-white hover:bg-[#2cbc8f]
                     hover:scale-105 transition-all duration-300 mr-5 md:mr-0
                     hidden md:block'
-                >
-                    Self-Care
-                </button>
+                    >
+                        Pay-Bill
+                    </button>
+                    <button
+                        className='px-4 py-2 bg-[#1c9a72] 
+                    rounded-md text-white hover:bg-[#2cbc8f]
+                    hover:scale-105 transition-all duration-300 mr-5 md:mr-0
+                    hidden md:block'
+                    >
+                        Self-Care
+                    </button>
+
+                    <div className="flex items-center gap-4 px-7">
+                        <IconButton
+                            variant="text"
+                            className=" md:hidden"
+                            ripple={false}
+                            onClick={() => setOpenNav(!openNav)}
+                        >
+                            {openNav ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    className="h-6 w-6"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            )}
+                        </IconButton>
+                    </div>
+                </div>
             </nav>
+
+            <DropdownMenu openNav={openNav} handleOpenNav={handleOpenNav} />
+
         </div>
     )
 }
